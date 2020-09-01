24bff3c035447a584d8b968af546704d809b938a
mr38 <ami.maruf.r@gmail.com>
2020-09-01 22:35:17 +0600

    completed bohubrihi html capstone project.

---
+++ bohubrihi-capstone-web/js/main.js
@@ -0,0 +1,10 @@
+  // close collapsed navbar when clicked outside 
+  $(document).ready(function () {
+    $(document).click(function (event) {
+        var click = $(event.target);
+        var _open = $(".navbar-collapse").hasClass("show");
+        if (_open === true && !click.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    });
+});
