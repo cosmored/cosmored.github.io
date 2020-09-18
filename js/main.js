f011dc5db1f30f06d3546aab094e8fed1dcac44e
mr38 <ami.maruf.r@gmail.com>
2020-09-18 10:27:44 +0600

    inlined initial script for better FCP score

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // resize the header section
-$(document).ready(function(){
-  $('.header').height($(window).height());
-  })
-  
-  // close collapsed navbar when clicked outside 
-  $(document).ready(function () {
+// $(document).ready(function(){
+//     $('.header').height($(window).height());
+// })
+
+// close collapsed navbar when clicked outside 
+$(document).ready(function () {
     $(document).click(function (event) {
         var click = $(event.target);
         var _open = $(".navbar-collapse").hasClass("show");
